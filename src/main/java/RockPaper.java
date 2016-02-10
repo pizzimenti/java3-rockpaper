edda025d128695809e75d8667a359e4fa7437379
Anna Becker and Brad Pizzimenti <anna.r.becker@gmail.com>
2016-02-10 11:20:41 -0800

    Add tests and functionality for player draw or player win.

--- src/main/java/RockPaper.java
+++ src/main/java/RockPaper.java
@@ -5,15 +5,47 @@ import spark.template.velocity.VelocityTemplateEngine;
 
 import static spark.Spark.*;
 
-public class RockPaper {
-  public static void main(String[] args) {
+import java.util.Random;
 
-  }
+public class RockPaper {
+  public static void main(String[] args) {}
+
+  
 
   public static Boolean checkPlayer1Wins(String player1, String player2) {
-    if (player1 == "Rock" && player2 == "Scissors") {
+    if ((player1.equalsIgnoreCase("Rock") && player2.equalsIgnoreCase("Scissors")) || (player1.equalsIgnoreCase("Paper") && player2.equalsIgnoreCase("Rock")) || (player1.equalsIgnoreCase("Scissors") && player2.equalsIgnoreCase("Paper"))) {
       return true;
     }
     return false;
   }
+
+  public static Boolean checkTie(String player1, String player2) {
+    if ((player1.equalsIgnoreCase("Rock") && player2.equalsIgnoreCase("Rock")) || (player1.equalsIgnoreCase("Paper") && player2.equalsIgnoreCase("Paper")) || (player1.equalsIgnoreCase("Scissors") && player2.equalsIgnoreCase("Scissors"))) {
+      return true;
+    }
+    return false;
+  }
+
+  public static String runGame(String player1, String player2) {
+    if (checkTie(player1, player2)) {
+      return "tie";
+    } else if (checkPlayer1Wins(player1, player2)) {
+      return "player1wins";
+    } else {
+    return "player2wins";
+    }
+  }
+  // runGame{
+  //   if(checktie()){
+  //     return "tie";
+  //   }
+  //   if (checkPlayer1Wins()) {
+  //     return "player1wins"
+  //   } else {
+  //     return "player2wins"
+  //   }
+  //
+  // }
+
+
 }
--- src/test/java/RockPaperTest.java
+++ src/test/java/RockPaperTest.java
@@ -4,9 +4,35 @@ import org.junit.*;
 public class RockPaperTest {
 
   @Test
-  public void checkPlayer1Wins_rockBeatsScissors_true() {
+  public void randomMove_assignMoveToRandomNumber_rock() {
     RockPaper testApp = new RockPaper();
-    assertEquals(true, testApp.checkPlayer1Wins("Rock","Scissors"));
+    // genrandom number
+    // assign that to computermove
+    // computer move to correctstring of move
+
+    assertEquals("rock", testApp.randomMove(0));
   }
 
+  @Test
+  public void checkPlayer1Wins_rockBeatsScissors_true() {
+    RockPaper testApp = new RockPaper();
+    assertEquals(true, testApp.checkPlayer1Wins("rock","scissors"));
+  }
+
+  @Test
+  public void checkTie_rockTiesRock_true() {
+    RockPaper testApp = new RockPaper();
+    assertEquals(true, testApp.checkTie("rock", "rock"));
+  }
+
+  @Test
+  public void runGame_paperTiesPaper_tie() {
+    RockPaper testApp = new RockPaper();
+    assertEquals("tie", testApp.runGame("paper","paper"));
+  }
+  @Test
+  public void endOfGame_rockLosesPaper_player2wins() {
+    RockPaper testApp = new RockPaper();
+    assertEquals("player2wins", testApp.runGame("rock","paper"));
+  }
 }
